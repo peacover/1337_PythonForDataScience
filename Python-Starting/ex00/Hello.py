fc30e4040e05a5b4636b50c2196daf08fff5200d
Youssef Er-raki <yer-raki@c4r7p8.1337.ma>
2023-09-05 19:44:17 +0100

    pyhton init

---
+++ Python-Starting/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Morocco!")
+ft_set = {"Hello", "Benguerir!"}
+ft_dict = {"Hello" : "1337-BG!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
